0000747f5fb0bc041a47a8028c73af0959d3996f
Eric Nilsson <eric.nilsson931211@gmail.com>
2016-05-18 18:54:52 +0200

    second commit

--- app/config/config_mysql.php
+++ app/config/config_mysql.php
@@ -1,11 +1,10 @@
 <?php
 
 return [
-    // Set up details on how to connect to the database
-    // Set up details on how to connect to the database
-    'dsn'     => "mysql:host=blu-ray.student.bth.se; dbname=erna13",
-    'username'        => "erna13",
-    'password'        => "8F77Kb5P7JnyZs7C",
+
+    'dsn'     => "mysql:host=localhost;dbname=phpmvc;",
+    'username'        => "",
+    'password'        => "",
     'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
     'table_prefix'    => "phpmvc",
 
@@ -14,25 +13,8 @@ return [
 
     // Throw a more verbose exception when failing to connect
     'debug_connect' => 'true',
+
 ];
-/*
-*På server**
-*
-*
-'dsn'     => "mysql:host=blu-ray.student.bth.se; dbname=erna13",
-'username'        => "erna13",
-'password'        => "8F77Kb5P7JnyZs7C",
-'driver_options'  => [PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"],
-'table_prefix'    => "phpmvc",
-
-// Display details on what happens
-'verbose' => false,
-
-// Throw a more verbose exception when failing to connect
-'debug_connect' => 'true',
-*/
-
-
 
 
 /*
